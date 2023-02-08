684c63b9a0eee7858efbdee123abf2ada5f52bd0
maumlima <mauriciodemouralima@gmail.com>
2023-02-08 17:50:16 +0100

    change readme

--- RLProject_mao.py
+++ RLProject_mao.py
@@ -1,11 +1,14 @@
 import numpy as np
 import gym
+import os
+import sys
 from gym.wrappers import RecordVideo
 from stable_baselines3 import DQN, DDPG, PPO
 from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.noise import NormalActionNoise
 from stable_baselines3.common.vec_env import SubprocVecEnv
 
+sys.path.insert(1, "./highway-env")
 import highway_env
 
 import pprint
@@ -14,7 +17,7 @@ import pprint
 TRAIN = False
 
 if __name__ == '__main__':
-    n_cpu = 6
+    n_cpu = 4
     batch_size = 64
     env = make_vec_env("racetrack-v0", n_envs=n_cpu, vec_env_cls=SubprocVecEnv)
     model = PPO("MlpPolicy",
@@ -39,11 +42,52 @@ if __name__ == '__main__':
     env = gym.make("racetrack-v0")
     
     # additional configurations
-    env.config["other_vehicles"] = 4
-    env.config["manual_control"] = True
+    # Config environment
+    env.configure({
+        "collision_reward": -2,
+        "lane_centering_cost": 3,
+        "lane_centering_reward": 1,
+        "controlled_vehicles": 3,
+        "other_vehicles": 2,
+        "screen_width": 800,
+        "show_trajectories": True,
+        "screen_heigth": 600
+    })
 
-    # test
-    pprint.pprint(env.config)
+    env.configure({
+        "action": {
+            "type": "MultiAgentAction",
+                "action_config": {
+                        "type": "ContinuousAction",
+                        "longitudinal": False,
+                        "lateral": True,
+                        "target_speeds": [0, 5, 10]  
+                        }
+        },
+    })
+
+    #config multi-observation
+    env.configure({
+        "observation": {
+            "type": "MultiAgentObservation",
+                "observation_config": {
+                    "type": "OccupancyGrid",
+                    "features": ['presence', 'on_road'],
+                    "grid_size": [[-18, 18], [-18, 18]],
+                    "grid_step": [3, 3],
+                    "as_image": False,
+                    "align_to_vehicle_axes": True
+                    },
+            }
+    })
+
+    # for manual control
+    if MANUAL:
+        env.config["action"]["longitudinal"] = True
+        env.config["manual_control"] = False
+
+    # allows one vehicule to be controlled
+    env.config["manual_control"]= True
 
     env = RecordVideo(env, video_folder="racetrack_ppo/videos", episode_trigger=lambda e: True)
     env.unwrapped.set_record_video_wrapper(env)
@@ -52,10 +96,22 @@ if __name__ == '__main__':
         done = truncated = False
         obs, info = env.reset()
         while not (done or truncated):
-            # Predict
-            action, _states = model.predict(obs, deterministic=True)
-            # Get reward
-            obs, reward, done, truncated, info = env.step(action)
-            # Render
-            env.render()
+            ### MONO AGENT
+            # # Predict
+            # action, _states = model.predict(obs, deterministic=True)
+            # # Get reward
+            # obs, reward, done, truncated, info = env.step(action) # wbefore it was not 0.01 but action
+            # # Render
+            # env.render()
+
+            ### MULTI AGENT
+            print(obs)
+            # Dispatch the observations to the model to get the tuple of actions
+            action = tuple(model.predict(obs_i) for obs_i in obs)
+            # Execute the actions
+            next_obs, reward, done, truncated, info = env.step(action)
+            # Update the model with the transitions observed by each agent
+            for obs_i, action_i, next_obs_i in zip(obs, action, next_obs):
+                model.update(obs_i, action_i, next_obs_i, reward, info, done, truncated)
+
     env.close()
