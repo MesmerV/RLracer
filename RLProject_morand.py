361ccefe8d79edfc40de074bf7cf7cb8120ff4d5
MesmerV <victor.pccb@gmail.com>
2023-02-11 11:59:39 +0100

    flexible multi-agents simulation

--- RLProject_morand.py
+++ RLProject_morand.py
@@ -20,11 +20,76 @@ warnings.filterwarnings("ignore")
 TRAIN = False
 MANUAL = False
 
+def CreateEnv():
+    # dl racetrack as baseline
+    env = gym.make("racetrack-v0")
+
+    
+    # General Config
+    env.configure({"collision_reward": -2,
+                    "lane_centering_cost": 3,
+                    "lane_centering_reward": 1,
+                    "controlled_vehicles": 1,
+                    "other_vehicles": 5,
+                    "screen_width": 800,
+                    "show_trajectories": False,
+                    "screen_heigth": 600})
+    
+    env.configure({
+        "action": {
+                    "type": "ContinuousAction",
+                    "longitudinal": False,
+                    "lateral": True,
+                    "target_speeds": [0, 5, 10]   
+                },
+          })
+    
+
+    # for manual control
+    if MANUAL:
+        env.config["action"]["longitudinal"] = False
+        env.config["manual_control"] = False
+
+    #apply changes
+    env.reset()
+    
+    
+    return env
+
+def ConfigureMultiAgent(env,agent_num):
+    
+    #configure several agents
+    env.configure({ "controlled_vehicles": agent_num })
+    
+    
+    #get config for one agent
+    action_config = env.config["action"]
+    obs_config = env.config["observation"]
+    
+    
+    
+    #multi-action config
+    env.configure({
+        "action": {
+                "type": "MultiAgentAction",
+                    "action_config": action_config
+            },
+          })
+    
+    #config multi-observation
+    env.configure({
+        "observation": {
+                "type": "MultiAgentObservation",
+                    "observation_config": obs_config
+                }
+            })  
+
 if __name__ == '__main__':
 
     n_cpu = os.cpu_count() - 1
     env = make_vec_env("racetrack-v0", n_envs=n_cpu, vec_env_cls=SubprocVecEnv)
-    
+    #env = CreateEnv()
+    n_cpu = 1
 
     #If train, create new model and train it
     if TRAIN:
@@ -41,75 +106,16 @@ if __name__ == '__main__':
                 tensorboard_log="racetrack_ppo/")
 
         # Train the model
-        model.learn(total_timesteps=int(1e5))
-        model.save("racetrack_ppo/model_morand")
+        model.learn(total_timesteps=int(1e2))
+        model.save("racetrack_ppo/model_test")
         del model
 
     # Run the algorithm
     model = PPO.load("racetrack_ppo/model_morand", env=env)
-    env = gym.make("racetrack-v0")
-
     
-    # Config environment
-    env.configure({"collision_reward": -2,
-                    "lane_centering_cost": 3,
-                    "lane_centering_reward": 1,
-                    "controlled_vehicles": 2,
-                    "other_vehicles": 4,
-                    "screen_width": 800,
-                    "show_trajectories": True,
-                    "screen_heigth": 600})
-    
-    """
-    env.configure({
-        "action": {
-                    "type": "ContinuousAction",
-                    "longitudinal": False,
-                    "lateral": True,
-                    "target_speeds": [0, 5, 10]  
-                    },
-          })
-    
-    
-    """
-    env.configure({
-        "action": {
-                "type": "MultiAgentAction",
-                    "action_config": {
-                            "type": "ContinuousAction",
-                            "longitudinal": False,
-                            "lateral": True,
-                            "target_speeds": [0, 5, 10]  
-                            }
-            },
-          })
-
-    #config multi-observation
-    env.configure({
-        "observation": {
-                "type": "MultiAgentObservation",
-                    "observation_config": {
-                        "type": "OccupancyGrid",
-                        "features": ['presence', 'on_road'],
-                        "grid_size": [[-18, 18], [-18, 18]],
-                        "grid_step": [3, 3],
-                        "as_image": False,
-                        "align_to_vehicle_axes": True
-                        },
-                }
-            })
-    
-
-    # for manual control
-    if MANUAL:
-        env.config["action"]["longitudinal"] = False
-        env.config["manual_control"] = False
-
-
-
-    #apply changes
-    env.reset()
-    
+    # dl racetrack as baseline
+    env = CreateEnv()
+    ConfigureMultiAgent(env,3)
 
     env = RecordVideo(env, video_folder="racetrack_ppo/videos", episode_trigger=lambda e: True)
     env.unwrapped.set_record_video_wrapper(env)
