6d3724c5a2b86289650626bd736a11d35372d413
Thomas C <82162924+alphanoxi@users.noreply.github.com>
2023-02-16 14:37:06 +0100

    function

--- RLProject_thomas.py
+++ RLProject_thomas.py
@@ -10,20 +10,22 @@ from torch.utils.tensorboard import SummaryWriter
 import warnings
 warnings.filterwarnings("ignore")
 learning_rate = 1e-3
-n_episode = 5000
+n_episode = 100
 # Définir le modèle de l'agent
 class model(nn.Module):
     def __init__(self, n_observations, n_actions):
         super(model, self).__init__()
         self.n_input = n_observations
-        self.layer1 = nn.Linear(n_observations, 128)
-        self.layer2 = nn.Linear(128, 256)
-        self.layer3 = nn.Linear(256, 512)
-        self.layer4 = nn.Linear(512, 512)
-        self.layer5 = nn.Linear(512, 512)
-        self.layer6 = nn.Linear(512, 256)
-        self.layer7 = nn.Linear(256,128)
-        self.layer8 = nn.Linear(128, n_actions)
+        self.layer1 = nn.Linear(n_observations, 64)
+        self.layer2 = nn.Linear(64, 128)
+        self.layer3 = nn.Linear(128, 128)
+        self.layer4 = nn.Linear(128, 256)
+        self.layer5 = nn.Linear(256,256)
+        self.layer6 = nn.Linear(256, 128)
+        self.layer7 = nn.Linear(128, 64)
+        self.layer8 = nn.Linear(64,16)
+        self.layer9 = nn.Linear(16,16)
+        self.layer10 = nn.Linear(16, n_actions)
 
     # Called with either one element to determine next action, or a batch
     # during optimization. Returns tensor([[left0exp,right0exp]...]).
@@ -37,8 +39,9 @@ class model(nn.Module):
         x = F.relu(self.layer5(x))
         x = F.relu(self.layer6(x))
         x = F.relu(self.layer7(x))
-
-        return self.layer8(x)
+        x = F.relu(self.layer8(x))
+        x = F.relu(self.layer9(x))
+        return self.layer10(x)
 class A2C():
     def __init__(self, n_observations, n_actions):
         self.actor = model(n_observations, n_actions).to(device)
@@ -51,12 +54,13 @@ class A2C():
     
     def get_action(self,state, episode):
         
-        std_v = 1 -0.5 *episode/n_episode
+        #std_v = 0.9 - 0.5 *np.min([episode/100,1])
+        std_v = 0.9 - 0.5 *episode/n_episode
         accel, angle = self.actor(state)[0]
         accel =  torch.normal(accel, std=std_v)
-        accel = torch.clamp(accel, -5, 5)
+        accel = torch.clamp(accel, -4, 4)
 
-        std_theta = 0.3 - (0.02-0.3)*episode/n_episode
+        std_theta =  0.3 - (0.05-0.3)*episode/n_episode
         angle =  torch.normal(angle, std=std_theta)
         angle = torch.clamp(angle, -0.78, 0.78)
 
@@ -84,110 +88,74 @@ class A2C():
         return policy_loss
 
 # Initialiser l'environnement
+def get_env():
+    env = gym.make("racetrack-v0")
+    env.config["controlled_vehicles"] = 1
+    env.config["manual_control"]= False
+    env.config["duration"] =12
+    env.config["lane_centering_cost"] = 10
+    env.config['other_vehicles']= 0
+    env.config["action"] = {
+                "type": "ContinuousAction",
+                "longitudinal":True,
+                "lateral": True,
+                "target_speeds": [0, 5, 10,15,20]
+            }
+    env.config["observation"] = {
+                        "type": "Kinematics",
+                        "features": [ "x", "y", "vx", "vy", "cos_h", "sin_h","long_off", "lat_off"],
+                        "features_range": {
+                            "x": [-100, 100],
+                            "y": [-100, 100],
+                            "vx": [-20, 20],
+                            "vy": [-20, 20]
+                        },
+                        "absolute": False,
+                        "order": "sorted"
 
-device = "cuda"
-env = gym.make("racetrack-v0")
-env.config["controlled_vehicles"] = 1
-env.config["manual_control"]= False
-env.config["duration"] =10
-env.config["lane_centering_cost"] = 10
-env.config['other_vehicles']= 0
-env.config["action"] = {
-            "type": "ContinuousAction",
-            "longitudinal":True,
-            "lateral": True,
-            "target_speeds": [0, 5, 10,15,20]
         }
-env.config["observation"] = {
-                    "type": "Kinematics",
-                    "features": ['presence', "x", "y", "vx", "vy", "long_off", "lat_off"],
-                    "features_range": {
-                        "x": [-100, 100],
-                        "y": [-100, 100],
-                        "vx": [-20, 20],
-                        "vy": [-20, 20]
-                    },
-                    "absolute": False,
-                    "order": "sorted"
-
-    }
+    return env
+device = "cuda"
+env= get_env()
 obs, info = env.reset()
 try:
     n1, n2, n3 = obs.shape
     state_size = n1*n2*n3
 except:
     n1, n2 = obs.shape
-    state_size = n1*n2
+    state_size = n2*2
 action_size = 2
-
-
-
-
-
-
-
-
-
 
     
 torch.set_grad_enabled(True)
 def train(agent, seed):
     
-    env = gym.make("racetrack-v0")
-    env.config["controlled_vehicles"] = 1
-    env.config["manual_control"]= False
-    env.config["duration"] =10
-    env.config["lane_centering_cost"] = 10
-    env.config['other_vehicles']= 0
-    env.config["action"] = {
-                    "type": "ContinuousAction",
-                    "longitudinal":True,
-                    "lateral": True,
-                    "target_speeds": [0, 5, 10,15,20]
-                }
-    env.config["observation"] = {
-                    "type": "Kinematics",
-                    "features": ['presence', "x", "y", "vx", "vy", "long_off", "lat_off"],
-                    "features_range": {
-                        "x": [-100, 100],
-                        "y": [-100, 100],
-                        "vx": [-20, 20],
-                        "vy": [-20, 20]
-                    },
-                    "absolute": False,
-                    "order": "sorted"
-
-    }
-
-
-
-
-    gains_totaux = []   
-    writer = SummaryWriter() 
+    env = env= get_env()
+    writer = SummaryWriter(f"runs/{seed}") 
     # Boucle d'entraînement de l'agent
     torch.manual_seed(seed)
     np.random.seed(seed)
     random.seed(seed)
     for i_episode in range(n_episode):
-        if (i_episode == 120):
-            env.config["duration"] = 30
-        if (i_episode == 300):
-            env.config["duration"] = 45
-        if (i_episode == 400):
-            env.config["duration"] = 60
-        if (i_episode == 450):
-            env.config["duration"] = 90
+        if (i_episode % 150 == 0):
+            env.config["duration"] = 25
         state, info = env.reset()
-        if (i_episode % 1000 == 0):
+        if (i_episode % 100 == 0):
             torch.save(agent.actor.state_dict(), "actor.pth")
             torch.save(agent.critic.state_dict(), "critic.pth")
-        state = torch.tensor(state, dtype=torch.float32)
+        
+        
+        state = state[0:2]
+        
+        
+        state = torch.tensor(state[0:2], dtype=torch.float32)
         done = False
         gains = []
         losses = []
         while not done:
             # Choisir une action
             long, lat = state[0][-2:]
+            state = state[0:2]
             state = torch.tensor(state, dtype=torch.float32)
             state = state.to(device)
             action_torch, action = agent.get_action(state, i_episode)
@@ -214,7 +182,7 @@ def train(agent, seed):
             policy_loss = agent.update_actor(q_value)
 
             state = next_state
-            
+            env.render()
             losses.append(q_value_loss.item())
             if (np.sqrt(long**2 + lat**2) > 70):
                 done = True
@@ -222,11 +190,12 @@ def train(agent, seed):
         writer.add_scalar("Rewards/train", np.mean(gains), i_episode)
 
     writer.close()
-
+    torch.save(agent.actor.state_dict(), "actor.pth")
+    torch.save(agent.critic.state_dict(), "critic.pth")
     
 
 if __name__ == '__main__':
-    processes = 5
+    processes = 1
     # initialiser une liste pour stocker les processus
     procs = []
     a2c =  A2C(state_size, action_size)
